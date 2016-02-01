73402fbf76edd83e590e626712fca59f310dbf26
Loic Baron <loic.baron@lip6.fr>
2016-02-01 17:55:13 +0100

    SfaAm started to implement ListResources and Describe, SfaReg comments about functions to implement

--- myslicelib/api/sfaam.py
+++ myslicelib/api/sfaam.py
@@ -1,4 +1,43 @@
 from myslicelib.api.sfa import Api as SfaApi
 
 class SfaAm(SfaApi):
-    pass
+
+    def get(self, obj_type, hrn):
+        try:
+            api_options = {}
+            if obj_type == 'resource':
+                result = self.ListResources([self.user_credential], api_options)
+                # filter and return the resource
+            elif obj_type == 'slice':
+                result = self.Describe([urn], self.slice_credential, api_options)
+            else:
+                raise NotImplementedError('Not implemented')
+        except Exception, e:
+            return False
+        return result
+    
+    def list(self, obj_type, hrn=None):
+        try:
+            api_options = {}
+            if hrn is None:
+                result = self.ListResources([self.user_credential], api_options)
+            else:
+                if self.version()['geni_api']==3:
+                    result = self.Describe([urn], self.slice_credential, api_options)
+                elif self.version()['geni_api']==2:
+                    api_options['geni_slice_urn'] = hrn_to_urn(hrn, 'slice')
+                    result = self.ListResources([urn], self.slice_credential, api_options)
+                else:
+                    raise NotImplementedError('Not implemented')
+        except Exception, e:
+            return False
+        return result
+
+
+        # self.Status
+        # self.Allocate
+        # self.Provision
+        # self.PerformOperationalAction
+        # self.Renew
+        # self.Shutdown
+        # self.Delete
--- myslicelib/api/sfareg.py
+++ myslicelib/api/sfareg.py
@@ -2,30 +2,34 @@ from myslicelib.api.sfa import Api as SfaApi
 
 class SfaReg(SfaApi):
 
+    def get(self, obj_type, hrn):
+        try:
+            result = self.Resolve(hrn, self.user_credential, {})
+            #result = filter_records(obj_type, result)
+        except Exception, e:
+            return False
+        return result
+    
+    def list(self, obj_type, hrn=None):
+        try:
+            if hrn is None:
+                # Registry must advertise hrn in GetVersion
+                # the hrn has to match the root authority
+                hrn = self.version['hrn']
+            result = self.List(hrn, self.credential, {})
+            #result = filter_records(obj_type, result)
+        except Exception, e:
+            return False
+        return result
+    
+    def user(self, hrn):
+        return self.get('user', hrn)
+    
+    def users(self, hrn=None):
+        return self.list('user', hrn)
 
-     def get(self, obj_type, hrn):
-         try:
-             result = self.Resolve(hrn, self.credential, {})
-             #result = filter_records(obj_type, result)
-         except Exception, e:
-             return False
-         return result
 
-     def list(self, obj_type, hrn=None):
-         try:
-             if hrn is None:
-                 # Registry must advertise hrn in GetVersion
-                 # the hrn has to match the root authority
-                 hrn = self.version['hrn']
-             result = self.List(hrn, self.credential, {})
-             #result = filter_records(obj_type, result)
-         except Exception, e:
-             return False
-         return result
-
-     def user(self, hrn):
-         return self.get('user', hrn)
-
-     def users(self, hrn=None):
-         return self.list('user', hrn)
-
+    # self.Register
+    # self.Update
+    # self.Remove
+    # self.CreateGid
