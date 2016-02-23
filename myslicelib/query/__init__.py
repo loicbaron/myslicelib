b2b6e4bba3ae25169683ae178a5b361cc21672ad
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-02-23 19:16:43 +0100

    myslicelib query interface base functionality

--- myslicelib/query/__init__.py
+++ myslicelib/query/__init__.py
@@ -1,10 +1,12 @@
-from myslicelib.api.planetlab import Api
-
+from myslicelib import setup as s
+from myslicelib.api import Api
 from myslicelib.model import Entities
 
-import logging
-
 class Query(object):
+
+    _entities = {
+        'Resources' : 'resource'
+    }
 
     # def __new__(cls, *args, **kwargs):
         # if cls is Query:
@@ -25,81 +27,89 @@ class Query(object):
         # else:
         #     return super(Query, cls).__new__(cls, *args, **kwargs)
 
-    def __init__(self, entity):
-        '''
-        dynamically loads the module and instantiate the class
-        self.cls will be the class to be used to populate the objects
-        '''
-        m, c = entity.split('.')
-        model = 'myslicelib.model.%s' % m
-        try:
-            module = __import__(model, fromlist=[c])
-        except ImportError:
-            logging.error("Model %s not found" % m)
-            exit(1)
+    def __init__(self, entities: Entities) -> None:
 
-        try:
-            self.cls = getattr(module, c)
-        except AttributeError as e:
-            logging.error(e)
-            logging.error("%s entity not found" % (entity.__name__))
-            exit(1)
+        if not entities.__name__ in self._entities:
+            raise NotImplementedError("Invalid object {} or not implemented".format(entities.__name__))
+
+        self.entities = entities
 
 
-        self.api = Api(
-                    'https://www.planet-lab.eu/PLCAPI/',
-                    'password',
-                    'myops@planet-lab.eu',
-                    '1:l-a)be'
-        )
+        self.api = getattr(Api(s.endpoints, s.credential), self._entities[entities.__name__])()
 
-        self.e = entity
-        self.f = {}
-        self.r = None
+        # self.api = Api(s.endpoints, s.credential)
+        # self.api = self.api.resource()
+
+        #for _e,_k in _entities:
+        # res = self.api.version()
+        #
+        # for k in res['ams']:
+        #     print("{} : ".format(k))
+        #
+        # m, c = entity.split('.')
+        # model = 'myslicelib.model.%s' % m
+        # try:
+        #     module = __import__(model, fromlist=[c])
+        # except ImportError:
+        #     logging.error("Model %s not found" % m)
+        #     exit(1)
+        #
+        # try:
+        #     self.cls = getattr(module, c)
+        # except AttributeError as e:
+        #     logging.error(e)
+        #     logging.error("%s entity not found" % (entity.__name__))
+        #     exit(1)
+        #
+        #
+        # self.api = Api(
+        #             'https://www.planet-lab.eu/PLCAPI/',
+        #             'password',
+        #             'myops@planet-lab.eu',
+        #             '1:l-a)be'
+        # )
+        #
+        # self.e = entity
+        # self.f = {}
+        # self.r = None
+
+    # def filter(self, key, value):
+    #     self.f[key] = value
+    #     return self
+    #
+    # def order(self, order):
+    #     return self.sort(order)
+    #
+    # def sort(self, sort):
+    #     return self.filter('-SORT', sort)
+    #
+    # def offset(self, offset):
+    #     if offset > 0:
+    #         return self.filter('-OFFSET', offset)
+    #     return self
+    #
+    # def limit(self, limit):
+    #     return self.filter('-LIMIT', limit)
+    #
+    # def gt(self, key, value):
+    #     return self.filter('>' + key, value)
+    #
+    # def gte(self, key, value):
+    #     return self.filter(']' + key, value)
+    #
+    # def lt(self, key, value):
+    #     return self.filter('<' + key, value)
+    #
+    # def lte(self, key, value):
+    #     return self.filter('[' + key, value)
+    #
+    # def find(self):
+    #     return self.execute()
 
 
 
-    def filter(self, key, value):
-        self.f[key] = value
-        return self
+    def get(self):
 
-    def order(self, order):
-        return self.sort(order)
+        res = self.api.get()
 
-    def sort(self, sort):
-        return self.filter('-SORT', sort)
-
-    def offset(self, offset):
-        if offset > 0:
-            return self.filter('-OFFSET', offset)
-        return self
-
-    def limit(self, limit):
-        return self.filter('-LIMIT', limit)
-
-    def gt(self, key, value):
-        return self.filter('>' + key, value)
-
-    def gte(self, key, value):
-        return self.filter(']' + key, value)
-
-    def lt(self, key, value):
-        return self.filter('<' + key, value)
-
-    def lte(self, key, value):
-        return self.filter('[' + key, value)
-
-    def find(self):
-        return self.execute()
-
-    def execute(self):
-
-        es = Entities()
-        for el in self.api.get(self.cls.__name__):
-            es.add(self.cls(el))
-        return es
-
-        es = []
-        for el in self.api.select(self.cls.__name__, self.f, self.r):
-            es.add(self.cls(el))
-        return es
+        print(res)
