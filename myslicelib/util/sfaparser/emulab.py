280f2796c57c2a5d8863d7b1fccbc8fe49628ef9
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-02-26 14:32:58 +0100

    sfaparser module for rspec parsing

---
+++ myslicelib/util/sfaparser/emulab.py
@@ -0,0 +1,14 @@
+def parser(rspec):
+    result = []
+
+    for node in rspec.findall('{http://www.geni.net/resources/rspec/3}node'):
+        resource = {
+            'type' : 'node',
+            'id': node.attrib['component_id'],
+            'hostname': node.attrib['component_name'],
+        }
+        for element in list(node):
+            if 'location' in element.tag:
+                resource['location'] = element.attrib
+        result.append(resource)
+    return result
