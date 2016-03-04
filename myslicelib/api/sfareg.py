72a0a5cd9f9d21a219ce2bb0d3e6e295a2369f80
Loic Baron <loic.baron@lip6.fr>
2016-03-04 16:13:11 +0100

    SfaReg don't open cert file if it's a string

--- myslicelib/api/sfareg.py
+++ myslicelib/api/sfareg.py
@@ -6,8 +6,9 @@ class SfaReg(SfaApi):
 
     def __init__(self, endpoint, credential):
         super(SfaReg, self).__init__(endpoint, credential)
-        with open(self.credential.certificate, "r") as myfile:
-            certificate = myfile.read()
+        if not isinstance(credential.certificate, str):
+            with open(self.credential.certificate, "r") as myfile:
+                certificate = myfile.read()
         self.user_credential = self._proxy.GetSelfCredential(
                                         certificate,
                                         self.credential.hrn,
