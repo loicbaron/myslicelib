784809d4c94c0584f7123056cab0bc65199ce263
Quan Zhou <quan.zhou@lip6.fr>
2016-03-15 14:28:32 +0100

    fix project query and update model

--- myslicelib/query/project.py
+++ myslicelib/query/project.py
@@ -1,4 +1,32 @@
+from myslicelib import setup as s
+from myslicelib.api import Api
+from myslicelib.model import Entity, Entities
 from myslicelib.query import Query
 
 class ProjectQuery(Query):
-    pass
+    
+    def __init__(self, entity: Entity) -> None:
+        self.entity = entity
+        self.api = getattr(Api(s.endpoints, s.credential), 'project')()
+        self.auth_api = getattr(Api(s.endpoints, s.credential), 'authority')()
+
+    def get(self):
+        res = self.api.get(self._id)
+        return self.collection(res)
+
+    def update(self, params):
+
+        if not self._id:
+            raise Exception("No element specified")
+
+        res = self.auth_api.update(self._id, params)
+
+        return self.collection(res)
+
+    def delete(self):
+        if not self._id:
+            raise Exception("No element specified")
+
+        res = self.auth_api.delete(self._id)
+
+        return res 
