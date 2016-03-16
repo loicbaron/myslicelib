3894f7c9429e2f4f87bd07aa42313a70c78bda2c
Loic Baron <loic.baron@lip6.fr>
2016-03-16 15:22:03 +0100

    Authority model addPi/removePi

--- myslicelib/model/authority.py
+++ myslicelib/model/authority.py
@@ -1,5 +1,6 @@
 import myslicelib
 
+from copy import copy
 from myslicelib.model import Entities, Entity
 from myslicelib.query import q
 
@@ -10,45 +11,28 @@ class Authority(Entity):
     _class = "Authority"
     _collection ="Authorities"
 
-    @property
-    def users(self):
+    def getUsers(self, pis = False):
         User = myslicelib.model.user.User
         result = []
-        for urn in self.attribute('users'):
-            result += q(User).id(urn).get()
+        if pis:
+            for urn in self.attribute('pi_users'):
+                result += q(User).id(urn).get()
+        else:
+            for urn in self.attribute('users'):
+                result += q(User).id(urn).get()
         return result
 
-    @users.setter
-    def users(self, value):
-        if not isinstance(value, list):
-            raise TypeError('a list needed to set')
-        self.setattribute('users', value)
-
-    @property
-    def pi_users(self):
-        User = myslicelib.model.user.User
-        result = []
-        for urn in self.attribute('pi_users'):
-            result += q(User).id(urn).get()
-        return result
-
-    @pi_users.setter
-    def pi_users(self, value):
-        if not isinstance(value, list):
-            raise TypeError('a list needed to set')
-        self.setattribute('pi_users', value)
-
-    @property
-    def slices(self):
+    def getSlices(self):
         Slice = myslicelib.model.slice.Slice
         result = []
         for urn in self.attribute('slices'):
             result += q(Slice).id(urn).get()
         return result
 
-    @slices.setter
-    def slices(self, value):
-        if not isinstance(value, list):
-            raise TypeError('a list needed to set')
-        self.setattribute('slices', value)
-        # TOD0: SFAAM UPDATE
+    def addPi(self, user):
+        self.pi_users.append(user.id)
+        return self
+
+    def removePi(self, user):
+        self.pi_users = set(self.pi_users) - set(user.id)
+        return self
