8adbedac825e223d6ee7ff437b4d4bb16de278bb
Loic Baron <loic.baron@lip6.fr>
2016-04-29 15:12:20 +0200

    Model Project: addPi/removePi to Project -> addUser/removeUser to Slices of the Project

--- myslicelib/model/project.py
+++ myslicelib/model/project.py
@@ -15,3 +15,18 @@ class Project(Authority):
     def getAuthority(self):
         auth_id = self.id.replace(':'+self.id.split('+')[1].split(':')[-1],'')
         return q(Authority).id(auth_id).get().first()
+
+    def addPi(self, user):
+        self.pi_users.append(user.id)
+        sl = self.getSlices()
+        for s in sl:
+            s.addUser(user)
+        return self
+
+    def removePi(self, user):
+        self.pi_users = set(self.pi_users) - set(user.id)
+        sl = self.getSlices()
+        for s in sl:
+            s.removeUser(user)
+        return self
+
