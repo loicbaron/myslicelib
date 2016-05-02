3f32f30c9f2256616ac5a4b1ff87c361b3c4d2c1
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2016-05-02 20:16:24 +0200

    fixed project __init__

--- myslicelib/query/project.py
+++ myslicelib/query/project.py
@@ -1,13 +1,18 @@
-from myslicelib import setup as s
+from myslicelib import setup as s, Setup
 from myslicelib.api import Api
 from myslicelib.model import Entity, Entities
 from myslicelib.query import Query
 
 class ProjectQuery(Query):
     
-    def __init__(self, entity: Entity) -> None:
-        super().__init__(entity)
-        self.auth_api = getattr(Api(s.endpoints, s.credential), 'project')()
+    def __init__(self, entity: Entity, setup=None) -> None:
+        super().__init__(entity, setup)
+
+        if setup and isinstance(setup, Setup):
+            _auth_setup = setup
+        else:
+            _auth_setup = s
+        self.auth_api = getattr(Api(_auth_setup.endpoints, _auth_setup.credential), 'project')()
 
     def update(self, params):
         if not self._id:
