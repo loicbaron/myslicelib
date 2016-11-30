52468c15f018ec4fffff916b39e8d02ba2836efd
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2016-11-30 16:47:21 +0100

    Endpoint: can specify list of technologies

--- myslicelib/util/__init__.py
+++ myslicelib/util/__init__.py
@@ -13,10 +13,11 @@ class Endpoint(object):
 
     """
 
-    def __init__(self, type="AM", protocol="SFA", url=None, name=None, timeout=None):
+    def __init__(self, type="AM", protocol="SFA", url=None, name=None, timeout=None, technologies=None):
         self.name = name
         self.type = type
         self.protocol = protocol
+        self.technologies = technologies.split(',')
         if timeout:
             self.timeout = timeout
         else:
